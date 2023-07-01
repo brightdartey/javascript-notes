043d601355446e44991ccaac8d840ade04bbfc28
brightdartey <darteyb28@gmail.com>
2023-07-01 15:29:15 +0000

    objects update

--- 9_objects.js
+++ 9_objects.js
@@ -95,3 +95,50 @@ function checkObj(checkProp) {
 console.log(checkObj("dog"));
 
 //manupulating complex objects
+
+//record collection
+
+ let collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": [ ]
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    },
+ };
+// keep a copy of the collection for test
+let collectionCopy = JSON.parse(JSON.stringify(collection));
+
+//Only change code below this line
+function updateRecords(id, prop, value) {
+    if (value === " ") {
+        delete collection[id][prop];
+    } else if (value === "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    } else {
+        collection[id][prop] = value;
+    }
+
+    return collection;
+};
+
+//Alter values below to test code
+console.log(updateRecords(5439, "artist", "ABBA"));
